9f1847c24a2270b8e2f174d1a8c32b55fd47c7f6
WindowGenerator <41333030+WindowGenerator@users.noreply.github.com>
2021-06-01 00:21:31 +0500

    рефакторинг кода + исправление небольших недочетов с стилях

--- src/app/core/services/favorites-data.service.ts
+++ src/app/core/services/favorites-data.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@angular/core';
+import {Injectable, OnDestroy, OnInit} from '@angular/core';
 import {ComponentPartsModel} from '../models';
 import {CommonInfoAboutBuildPc} from './component-data.service';
 
@@ -6,36 +6,17 @@ import {CommonInfoAboutBuildPc} from './component-data.service';
   providedIn: 'root'
 })
 export class FavoritesDataService {
+  // Максимально тупая реализация кэша, но надежная и понятная
+
+  private readonly favoritesCacheKey = 'favorites_components_parts';
 
   _favoritesItemIdToComponentsParts: { [key: number]: ComponentPartsModel } = {};
 
   constructor() {
   }
 
-  set favoritesComponentsParts(favoritesComponentsPartsParams: Array<ComponentPartsModel>) {
-    for (const newFavoriteComponentPart of favoritesComponentsPartsParams) {
-      const itemId = newFavoriteComponentPart.ItemId;
-      this._favoritesItemIdToComponentsParts[itemId] = newFavoriteComponentPart;
-    }
-
-    const toSave = JSON.stringify(this._favoritesItemIdToComponentsParts);
-    localStorage.setItem('favoritesComponentsParts', toSave);
-  }
-
-  get favoritesComponentsParts() {
-    const toLoad = localStorage.getItem('favoritesComponentsParts') || null;
-    if (toLoad !== null) {
-      this._favoritesItemIdToComponentsParts = JSON.parse(toLoad);
-    }
-    const favoritesItemIdToComponentsPartsArray: Array<ComponentPartsModel> = [];
-    for (const [_, componentPart] of Object.entries(this._favoritesItemIdToComponentsParts)) {
-      favoritesItemIdToComponentsPartsArray.push(componentPart);
-    }
-    return favoritesItemIdToComponentsPartsArray;
-  }
-
-  removeFavoriteElement(favoritesComponentItemId: number): void {
-    const toLoad = localStorage.getItem('favoritesComponentsParts') || null;
+  removeFavoriteElementFromCache(favoritesComponentItemId: number): void {
+    const toLoad = localStorage.getItem(this.favoritesCacheKey) || null;
     if (toLoad !== null) {
       this._favoritesItemIdToComponentsParts = JSON.parse(toLoad);
     }
@@ -46,6 +27,29 @@ export class FavoritesDataService {
     delete this._favoritesItemIdToComponentsParts[favoritesComponentItemId];
 
     const toSave = JSON.stringify(this._favoritesItemIdToComponentsParts);
-    localStorage.setItem('favoritesComponentsParts', toSave);
+    localStorage.setItem(this.favoritesCacheKey, toSave);
+  }
+
+  loadFavoritesFromCache(): Array<ComponentPartsModel> {
+    const toLoad = localStorage.getItem(this.favoritesCacheKey) || null;
+    if (toLoad !== null) {
+      this._favoritesItemIdToComponentsParts = JSON.parse(toLoad);
+    }
+    const favoritesItemIdToComponentsPartsArray: Array<ComponentPartsModel> = [];
+    for (const [_, componentPart] of Object.entries(this._favoritesItemIdToComponentsParts)) {
+      favoritesItemIdToComponentsPartsArray.push(componentPart);
+    }
+    return favoritesItemIdToComponentsPartsArray;
+  }
+
+  saveFavoritesToCache(favoritesComponentsPartsParams: Array<ComponentPartsModel>) {
+    this.loadFavoritesFromCache();
+    for (const newFavoriteComponentPart of favoritesComponentsPartsParams) {
+      const itemId = newFavoriteComponentPart.ItemId;
+      this._favoritesItemIdToComponentsParts[itemId] = newFavoriteComponentPart;
+    }
+
+    const toSave = JSON.stringify(this._favoritesItemIdToComponentsParts);
+    localStorage.setItem(this.favoritesCacheKey, toSave);
   }
 }
